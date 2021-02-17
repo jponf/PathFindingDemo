98e3eed0b0b2ae72854e0f9cedbab8a22e931f56
Josep Pon Farreny <jponfarreny@gmail.com>
2021-02-17 19:28:38 +0100

    Added project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const paths = {
+    src: path.resolve(__dirname, "src"),
+}
+
+module.exports = {
+    mode: "development",  // production | development | none
+    entry: {
+        index: "./src/index.ts",
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+            favicon: path.resolve(__dirname, "src", "assets", "index-favicon.ico"),
+            chunks: ["index"],
+        }),
+        // new  BundleAnalyzerPlugin(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, "src"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
